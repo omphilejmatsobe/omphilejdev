cb076c9d570b0077c8919d05634c932518ad2b83
OmphileMatsobe <omphilejmatsobe@gmail.com>
2024-03-14 00:30:10 +0200

    Files update

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( 100, 100 );
 
-container = document.getElementById('object');
+var container = document.getElementById('object');
 document.body.appendChild( container );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
