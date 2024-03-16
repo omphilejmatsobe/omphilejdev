877198fa15cc2eac5c5c10f0a44a243ebc62267a
OmphileMatsobe <omphilejmatsobe@gmail.com>
2024-03-16 17:42:33 +0200

    Application Update

--- toggle.js
+++ toggle.js
@@ -1,17 +1,20 @@
 var toggler = false;
 var toggleNav = document.getElementById('toggleNav');
+var toogleIcon = document.getElementById('toggleIcon');
 
 function toggle()
 {
     if (toggler == true)
     {
         toggleNav.classList.remove('active');
+        toggleIcon.classList.remove('active');
         console.log('off');
         toggler = false;
     }
     else if (toggler == false)
     {
         toggleNav.classList.add('active');
+        toggleIcon.classList.add('active');
         console.log('on');
         toggler = true;
     }
@@ -22,6 +25,7 @@ function offToggle()
     if (toggler == true)
     {
         toggleNav.classList.remove('active');
+        toggleIcon.classList.remove('active');
         console.log('off');
         toggler = false;
     }
