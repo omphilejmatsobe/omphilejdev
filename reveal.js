e38614e7fba2bd67b00e3a4cbd03b821eef4738d
OmphileMatsobe <omphilejmatsobe@gmail.com>
2024-03-18 15:51:47 +0200

    File check

--- reveal.js
+++ reveal.js
@@ -1,4 +1,5 @@
 window.addEventListener('scroll', reveal);
+var pageHeight = window.innerHeight;
 
 function reveal()
 {
@@ -6,7 +7,6 @@ function reveal()
 
   for (var x = 0; x < reveals.length; x++)
   {
-      var pageHeight = window.innerHeight;
       var revealTop = reveals[x].getBoundingClientRect().top;
       var revealPosition = 150;
 
@@ -15,4 +15,21 @@ function reveal()
         reveals[x].classList.add('active');
       }
     }
+}
+
+function change()
+{
+  var color = document.getElementsById('Navlinks');
+  var section = document.getElementById('about');
+
+  var sectionTop = section.getBoundingClientRect().top;
+
+  if (sectionTop < pageHeight)
+  {
+    color.classList.add('color');
+  }
+  else
+  {
+    color.classList.remove('color');
+  }
 }
