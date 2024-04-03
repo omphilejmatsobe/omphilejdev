679742ef2a2c2872e37c7fed13969af4704e93d5
OmphileMatsobe <omphilejmatsobe@gmail.com>
2024-04-03 05:14:56 +0200

    Curso Changed

---
+++ cursor.js
@@ -0,0 +1,21 @@
+
+    const cursor = document.querySelector(".cursor");
+    const outline = document.querySelector(".cursorOutline");
+
+    window.addEventListener("mousemove", 
+    function(e){
+        const posX = e.clientX;
+        const posY = e.clientY;
+
+        cursor.style.left = `${posX}px`;
+        cursor.style.top = `${posY}px`;
+
+        //outline.style.left = `${posX}px`;
+        //outline.style.top = `${posY}px`;
+
+        outline.animate({
+            top: `${posY}px`,
+            left: `${posX}px`
+        }, {duration: 4000, fill: "forwards"})
+    }
+    )
